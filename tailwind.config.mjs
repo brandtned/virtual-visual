29d61629675ef2ab31f99c0939e522dfb4b34290
Emma Bjørg <emso0005@stud.kea.dk>
2024-09-17 12:24:44 +0200

    farver

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			primary:'#012B27',
+			primarylight:'#0D3C2F',
+			secondary:'#629D97',
+			secondarylight:'DDEFFF',
+			accent:'FFDA03',
+		},
 		extend: {},
 	},
 	plugins: [],
